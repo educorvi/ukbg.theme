9b7950ad621ba1e89552e50176cf82c2c77d9c9b
stetrabby <st@abstract-technology.de>
2019-05-03 11:30:45 +0200

    moved rules for defining open portlet inside the js

--- ukbg/theme/static/assets/js/at-accordion.js
+++ ukbg/theme/static/assets/js/at-accordion.js
@@ -1,20 +1,24 @@
-// collapsible static portlet Bgetem Intranet
-var accordions = document.getElementsByClassName("accordion");
-var i;
+$(document).ready(function () {
 
-for (i = 0; i < accordions.length; i++) {
-  accordions[i].onclick = function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      // accordion is currently open, so close it
-      content.style.maxHeight = null;
-    } else {
-      // accordion is currently closed, so open it
-      content.style.maxHeight = content.scrollHeight + "px";
+    // collapsible static portlet Bgetem Intranet
+    var accordions = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < accordions.length; i++) {
+      accordions[i].onclick = function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          // accordion is currently open, so close it
+          content.style.maxHeight = null;
+        } else {
+          // accordion is currently closed, so open it
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      };
     }
-  };
-}
-// default open the first and the second accordion
-accordions[0].click();
-accordions[1].click();
+    // default open the first and the second accordion
+    accordions[0].click();
+    accordions[1].click();
+
+});
